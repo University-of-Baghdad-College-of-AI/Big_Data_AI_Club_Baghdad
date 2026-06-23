3be45180efd3fb0628a9520dac07b353bcc082a2
Jalal Sadoon Hameed <jalal.hameed@uobaghdad.edu.iq>
2026-06-23 22:59:36 +0300

    Update approved-users.js

--- approved-users.js
+++ approved-users.js
@@ -13,6 +13,7 @@ var APPROVED_USERS = {
 
   /* ── Professors ── */
   'jalal.hameed@uobaghdad.edu.iq': 'professor',
+  'jalal.hameed15@gmail.com': 'professor',
 
   /* ── Students ── */
   // 'student@gmail.com': 'student',
